c5b1a7d3c2812b40ac3af822557475dee648ab1e
mishkamebonia <mishkas.mebonia@gmail.com>
2024-03-10 22:11:14 +0400

    refactored

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,19 @@
 import styles from "./Header.module.scss";
+import { useTheme } from "../../providers/theme";
 
 const Header = () => {
+  const { theme, toggleTheme } = useTheme();
+
   return (
     <div className={styles.header}>
       <h1>devfinder</h1>
-      <button>
-        DARK <i className="fa-solid fa-moon"></i>
+      <button onClick={toggleTheme}>
+        <span>{theme}</span>{" "}
+        {theme === "dark" ? (
+          <i className="fa-solid fa-moon"></i>
+        ) : (
+          <i className="fa-solid fa-sun"></i>
+        )}
       </button>
     </div>
   );

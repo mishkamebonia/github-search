a8afa96fefac7156ab86958732c554c666c431bd
mishkamebonia <mishkas.mebonia@gmail.com>
2024-03-10 22:11:23 +0400

    refactored

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
+import "./index.scss";
+import "./scss/theme.scss";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import "./index.scss";
+import { ThemeProvider } from "./providers/theme.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
